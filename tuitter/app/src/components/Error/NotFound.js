c973015661c60604dd483762d7f7a22314980592
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2019-09-07 15:00:20 -0300

    Tuitter App
    Iniciando projeto Tuitter App

---
+++ tuitter/app/src/components/Error/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const NotFound = () => {
+    return (
+        <h1>Component de error 404</h1>
+    )
+
+}
---
+++ tuitter/app/src/routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Auth from '../auth/index'
+
+import { NotFound } from '../components/Error/NotFound'
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        Auth.isAuthenticated() ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{pathname: '/login', state: {from: props.location}}} />
+        )
+    )} />
+)
+
+export default function Routes() {
+    return (
+      <BrowserRouter>
+        <Switch>
+            <PrivateRoute exact path='/' component={() => <h1>Home</h1>}/>
+            <Route exact path='*' component={NotFound}/>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
