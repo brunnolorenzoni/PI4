ff58466f5fbeeaba6cf9cac2ec580d8ec7d6c460
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2019-09-09 20:51:12 -0300

    auth dev

--- tuitter/server/routes/auth.js
+++ tuitter/server/routes/auth.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
 
     const data = req.body;
 
+    const emailExist = await User.findOne({email: data.email});
+    if(emailExist) {
+        return res.status(400).send("Email já existe");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(data.password, salt);
+
     const user = new User({
         username: data.username,
         email: data.email,
-        password: data.password,
+        password: hashPassword,
         gender: data.gender,
     });
 
@@ -21,8 +31,28 @@ router.post('/register', async (req, res) => {
 
 });
 
-router.post('/login', (req, res) => {
-    res.send('Login');
+router.post('/login', async (req, res) => {
+    const data = req.body;
+
+    const user = await User.findOne({email: data.email});
+    if(!user) {
+        return res.status(400).send("Email não existe");
+    }
+
+    const validPassword = await bcrypt.compare(data.password, user.password);
+
+    if(!validPassword){
+        return res.status(400).send("Senha invalida");
+    }
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('token', token).send(token)
+
+});
+
+router.post('/auth', async (req, res) => {
+    const data = req.body;
+
 });
 
 module.exports = router;
